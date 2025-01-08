a8f25946e0e1f80f860a7ff450789633e1038a05
Beverly Hernandez <beverlyhernandez@vanessas-mbp.myfiosgateway.com>
2025-01-08 11:36:12 -0500

    add App.vue

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <the-header></the-header>
+  <div id="main">
+    <router-view :key="$route.fullPath"></router-view>
+  </div>
+  <the-footer></the-footer>
+</template>
+
+<script setup>
+import TheHeader from "./components/TheHeader.vue";
+import TheFooter from "./components/TheFooter.vue";
+</script>
+
+<style>
+#main {
+  margin-top: 12vh;
+  height: 80vh;
+
+  @media screen and (max-width: 768px) {
+    margin-top: 0;
+  }
+}
+</style>
