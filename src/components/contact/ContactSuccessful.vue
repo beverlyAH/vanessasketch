6caaf032b18f2b0a861cffa41679b75769e0fc50
Beverly Hernandez <beverlyandez@gmail.com>
2025-01-08 11:44:27 -0500

    add ContactSuccessful component

---
+++ src/components/contact/ContactSuccessful.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    id="contact-successful"
+    class="place-self-center border-2 p-3 m-auto w-1/2"
+  >
+    <h1 class="place-self-center">Message sent! Thank you!</h1>
+  </div>
+</template>
