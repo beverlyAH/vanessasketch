d57403b2b76d0ec571a2ca33542d8ff2358d1d72
Beverly Hernandez <beverlyandez@gmail.com>
2025-01-08 11:57:00 -0500

    add portfolio components

---
+++ src/components/portfolio/CardDeck.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="portfolio-listing bg-base-100">
+    <portfolio-card
+      categoryName="Personal"
+      imagePath="/portfolio_icons/Site_PersonalProject_Icon-768x768.png"
+      description="Art for personal projects."
+      routePath="/personal"
+    ></portfolio-card>
+    <portfolio-card
+      categoryName="Spongebob's Idle Adventures"
+      imagePath="/portfolio_icons/Site_SpongeBob_Icon-768x768.png"
+      description="Art and assets for Kongregate's Spongebob's Idle Adventures."
+      routePath="/spongebob"
+    ></portfolio-card>
+    <portfolio-card
+      categoryName="Environmental Art"
+      imagePath="/portfolio_icons/Site_EnviArt_Icon-768x768.png"
+      description="Art meant to evoke a sense of place or mood."
+      routePath="/environment"
+    ></portfolio-card>
+    <portfolio-card
+      categoryName="Protected"
+      imagePath="/portfolio_icons/Site_Restricted_Icon-768x768.png"
+      description="If you are a potential employer, please use the provided password."
+      routePath="/hops?protected=true"
+    ></portfolio-card>
+  </div>
+</template>
+
+<script setup>
+import PortfolioCard from "./PortfolioCard.vue";
+</script>
+
+<style scoped>
+.portfolio-listing {
+  margin-top: 12vh;
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+  padding: 10px;
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/components/portfolio/PortfolioCard.vue
@@ -0,0 +1,68 @@
+<template>
+  <div
+    class="h-fit portfolio-category bg-base-100 text-neutral-content items-center mb-5"
+  >
+    <router-link :to="categoryPath"
+      ><img
+        class="portfolio-listing-image align-self-center"
+        :src="props.imagePath"
+        :alt="props.categoryName"
+    /></router-link>
+    <h3>
+      <router-link :to="categoryPath">{{ props.categoryName }}</router-link>
+    </h3>
+    <p class="text-sm">
+      {{ props.description }}
+    </p>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps([
+  "categoryName",
+  "imagePath",
+  "routePath",
+  "description",
+]);
+
+const categoryPath = computed(() => {
+  return "/portfolio" + props.routePath;
+});
+</script>
+
+<style scoped>
+.portfolio-category {
+  display: inline-block;
+  width: 20rem;
+  margin: 0.5rem;
+  justify-content: space-between;
+  border-radius: 25px;
+  background-color: #340534;
+  padding: 0 0 10px 0;
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+    width: 90vw;
+  }
+}
+.portfolio-category,
+p,
+h3 {
+  padding: 0.5rem;
+}
+
+img {
+  margin-bottom: 0.5rem;
+  object-fit: cover;
+  object-position: center center;
+}
+.portfolio-listing-image {
+  width: 100%;
+  height: 20rem;
+  border-radius: 25px;
+}
+</style>
+
