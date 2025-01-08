11ae6a182cec6440dda7158de1dacc7bea60b1be
Beverly Hernandez <beverlyhernandez@vanessas-mbp.myfiosgateway.com>
2025-01-08 11:36:49 -0500

    add data

---
+++ src/data.js
@@ -0,0 +1,336 @@
+export default {
+    descriptions: {
+      spongebob: `<b>Role:</b> Game Artist</br>
+                  <b>Developer:</b> Kongregate</br>
+                  <b>Client:</b> Nickelodeon</br>
+                  <b>Tools:</b> Adobe Photoshop, Clip Studio Paint`,
+      environment: 'Environment excercises and studies',
+      personal: 'Currently, my approach to character design.',
+      hops: `<b>Company:</b>  Kongregate</br>
+            <b>Client:</b> (should be obvious)</br>
+            <b>Role:</b> Game Artist</br>
+            <b>Tools Used:</b> Clip Studio Paint, Adobe Photoshop</br>
+            <p>To protect NDA status, all identifying information has been omitted.</p>
+            <p>ARTWORK © IP HOLDER</p>`,
+    },
+    environment: [
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Site_Snowy_Village_Web-400x400.png',
+        fileFullSize: 'Site_Snowy_Village_Web.png',
+        title: 'Snowy Village House',
+        date: '2024',
+        description: ''
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Site_SciFi2_Web-400x400.png',
+        fileFullSize: 'Site_SciFi2_Web.png',
+        title: 'Sci-fi Alley',
+        date: '2024',
+        description: ''
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Site_AW2_Alley-400x400.png',
+        fileFullSize: 'Site_AW2_Alley.png',
+        title: 'Alan Wake 2 Alley',
+        date: '2024',
+        description: ''
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'SH2HallFinalWeb2-400x400.png',
+        fileFullSize: 'SH2HallFinalWeb2.png',
+        title: 'Silent Hill 2 Hallway Encounter',
+        date: '2024',
+        description: ''
+      },
+    ],
+    personal: [
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'AE_ZarasDirrek_Thumbnails-400x400.png',
+        fileFullSize: 'AE_ZarasDirrek_Thumbnails.png',
+        title: 'AE Zaras Dirrek',
+        date: '2024',
+        description: 'Step 1 - Thumbnails'
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'AE_ZarasDirrek_Concepts_01-400x400.png',
+        fileFullSize: 'AE_ZarasDirrek_Concepts_01.png',
+        title: 'AE Zaras Dirrek',
+        date: '2024',
+        description: 'Step 2 - Concepts, 1st Round'
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'AE_ZarasDirrek_Concept_02-400x400.png',
+        fileFullSize: 'AE_ZarasDirrek_Concept_02.png',
+        title: 'AE Zaras Dirrek',
+        date: '2024',
+        description: 'Step 2 - Concepts, 2nd Round'
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'AE_ZarasDirrek_Concept_Color-400x400.png',
+        fileFullSize: 'AE_ZarasDirrek_Concept_Color.png',
+        title: 'AE Zaras Dirrek',
+        date: '2024',
+        description: 'Step 3 - Color Variations'
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'AE_ZarasDirrek_Concept_Turnaround-400x400.png',
+        fileFullSize: 'AE_ZarasDirrek_Concept_Turnaround.png',
+        title: 'AE Zaras Dirrek',
+        date: '2024',
+        description: 'Step 4 - Turnaround'
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'AE_ZarasDirrek_Concept_KeyArt-400x400.png',
+        fileFullSize: 'AE_ZarasDirrek_Concept_KeyArt.png',
+        title: 'AE Zaras Dirrek',
+        date: '2024',
+        description: 'Step 5 - Key Art'
+      },
+    ],
+    hops: [
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Site_Castle_Flying-800x450.png',
+        fileFullSize: 'Site_Castle_Flying.png',
+        title: '',
+        date: '2024',
+        description: ''
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Site_Castle_Desert_01-800x450.png',
+        fileFullSize: 'Site_Castle_Desert_01.png',
+        title: '',
+        date: '2024',
+        description: ''
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Site_Castle_Desert_02-800x450.png',
+        fileFullSize: 'Site_Castle_Desert_02.png',
+        title: '',
+        date: '2024',
+        description: ''
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Site_Castle_Canyon_01-800x450.png',
+        fileFullSize: 'Site_Castle_Canyon_01.png',
+        title: '',
+        date: '2024',
+        description: ''
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Site_Castle_Canyon_02-800x450.png',
+        fileFullSize: 'Site_Castle_Canyon_02.png',
+        title: '',
+        date: '2024',
+        description: ''
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Site_Card_Arena_Interior-800x450.png',
+        fileFullSize: 'Site_Card_Arena_Interior.png',
+        title: '',
+        date: '2024',
+        description: ''
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Site_Anchor_Point-800x450.png',
+        fileFullSize: 'Site_Anchor_Point.png',
+        title: '',
+        date: '2024',
+        description: ''
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Site_Anchor_Interior-796x450.png',
+        fileFullSize: 'Site_Anchor_Interior.png',
+        title: '',
+        date: '2024',
+        description: ''
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'G_Card_Color_08-2-215x300.png',
+        fileFullSize: 'G_Card_Color_08-2.png',
+        title: 'Character 1',
+        date: '2024',
+        description: ''
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'U_Card-2-215x300.png',
+        fileFullSize: 'U_Card-2.png',
+        title: 'Character 2',
+        date: '2024',
+        description: ''
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'T_Card_07-215x300.png',
+        fileFullSize: 'T_Card_07.png',
+        title: 'Character 3',
+        date: '2024',
+        description: ''
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'M1_Card_Color_03-2-215x300.png',
+        fileFullSize: 'M1_Card_Color_03-2.png',
+        title: 'Character 4',
+        date: '2024',
+        description: ''
+      },
+    ],
+    spongebob: [
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Holiday_HolidayCoralTree-150x150.png',
+        fileFullSize: 'Holiday_HolidayCoralTree.png',
+        title: 'Holiday Coral Tree',
+        date: '2023',
+        description: ''
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Holiday_UI_Icons_SnowFlakes-150x150.png',
+        fileFullSize: 'Holiday_UI_Icons_SnowFlakes.png',
+        title: 'Holiday UI Icon',
+        date: '2023',
+        description: 'Snowflake'
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Holiday_UI_Icons_NicksCookie-150x150.png',
+        fileFullSize: 'Holiday_UI_Icons_NicksCookie.png',
+        title: 'Holiday UI Icon',
+        date: '2023',
+        description: 'Nick\'s Cookie'
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Holiday_UI_Icons_MacGuffinGenerator-150x150.png',
+        fileFullSize: 'Holiday_UI_Icons_MacGuffinGenerator.png',
+        title: 'Holiday UI Icon',
+        date: '2023',
+        description: '"MacGuffin Generator"'
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Holiday_UI_Icons_IcePops-150x150.png',
+        fileFullSize: 'Holiday_UI_Icons_IcePops.png',
+        title: 'Holiday UI Icon',
+        date: '2023',
+        description: 'Ice Pops'
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Holiday_UI_Icons_ColdSlicks-150x150.png',
+        fileFullSize: 'Holiday_UI_Icons_ColdSlicks.png',
+        title: 'Holiday UI Icon',
+        date: '2023',
+        description: 'Cold Slicks Ice Skate'
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Holiday_Snowman_Prop_Render-150x150.png',
+        fileFullSize: 'Holiday_Snowman_Prop_Render.png',
+        title: 'Holiday Snowman Prop',
+        date: '2023',
+        description: ''
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Holiday_WreathsNMore_L_Investment_Render-150x150.png',
+        fileFullSize: 'Holiday_WreathsNMore_L_Investment_Render.png',
+        title: 'Holiday Building',
+        date: '2023',
+        description: 'Wreaths n\' More (Investment, L)'
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Holiday_WreathsNMore_R_Investment_Render-150x150.png',
+        fileFullSize: 'Holiday_WreathsNMore_R_Investment_Render.png',
+        title: 'Holiday Building',
+        date: '2023',
+        description: 'Wreaths n\' More (Investment, R)'
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Valentine-_Prop_Heart_Lampposts-150x150.png',
+        fileFullSize: 'Valentine-_Prop_Heart_Lampposts.png',
+        title: 'Valentine Prop',
+        date: '2023',
+        description: 'Heart Lampposts'
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Valentine-_Prop_Heart_Bush-150x150.png',
+        fileFullSize: 'Valentine-_Prop_Heart_Bush.png',
+        title: 'Valentine Prop',
+        date: '2023',
+        description: 'Heart Bush'
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Valentine-_Prop_Heart_Bench-150x150.png',
+        fileFullSize: 'Valentine-_Prop_Heart_Bench.png',
+        title: 'Valentine Prop',
+        date: '2023',
+        description: 'Heart Bench'
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Valentine-_Prop_BoxofChocolates-150x150.png',
+        fileFullSize: 'Valentine-_Prop_Heart_BoxofChocolates.png',
+        title: 'Valentine Prop',
+        date: '2023',
+        description: 'Box of Chocolates'
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Cave_Prehistoric-150x150.png',
+        fileFullSize: 'Cave_Prehistoric.png',
+        title: 'Prehistoric Prop',
+        date: '2023',
+        description: 'Building, Cave (Investment, L)'
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Prehistoric_Prop_Rocks-150x150.png',
+        fileFullSize: 'Prehistoric_Prop_Rocks.png',
+        title: 'Prehistoric Prop',
+        date: '2023',
+        description: 'Rocks'
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Prehistoric_Prop_Kelp-150x150.png',
+        fileFullSize: 'Prehistoric_Prop_Kelp.png',
+        title: 'Prehistoric Prop',
+        date: '2023',
+        description: 'Kelp'
+      },
+      {
+        id: new Date().toISOString(),
+        fileThumbnail: 'Prehistoric_Prop_Grass-150x150.png',
+        fileFullSize: 'Prehistoric_Prop_Grass.png',
+        title: 'Prehistoric Prop',
+        date: '2023',
+        description: 'Grass'
+      },
+    ]
+}
