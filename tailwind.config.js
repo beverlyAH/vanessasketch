5c6232d3cb92b3482a3a0ea8e908f7272200833e
Beverly Hernandez <beverlyandez@gmail.com>
2025-01-08 16:20:22 -0500

    change require to import

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
     },
   },
   plugins: [
-    require('daisyui', require("tailgrids/plugin")),
+    import('daisyui', import("tailgrids/plugin")),
   ],
   daisyui: {
     themes: [
