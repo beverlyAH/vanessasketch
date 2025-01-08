f1645ae9fc3129a305c77e51fc688f7ee3c3235f
Beverly Hernandez <beverlyandez@gmail.com>
2025-01-08 17:07:32 -0500

    add postcss plugins to config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build: {
     commonjsOptions: { transformMixedEsModules: true } 
+  },
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
+    },
   }
 })
