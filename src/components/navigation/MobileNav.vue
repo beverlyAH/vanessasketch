090d945f4387cf48cfa00edd5cb135e38ce41eb5
Beverly Hernandez <beverlyandez@gmail.com>
2025-01-08 11:56:34 -0500

    add navbar components

---
+++ src/components/navigation/MobileNav.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="grid max-w-fit">
+    <img
+      class="header-logo align-self-center"
+      src="../../assets/vanessasketch_logo_mobile.png"
+      alt="Vanessa Cohen - illustration and concept art"
+    />
+    <ul class="menu bg-base-100 rounded-box">
+      <li>
+        <details class="max-w-1/2">
+          <summary>
+            <router-link to="/portfolio"> Portfolio </router-link>
+          </summary>
+          <ul class="bg-base-100 rounded-box">
+            <li>
+              <router-link to="/portfolio/personal">
+                Personal Projects
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/portfolio/spongebob">
+                SpongeBob's Idle Adventures
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/portfolio/environment">
+                Environmental Art
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/portfolio/hops?protected=true">
+                Protected
+              </router-link>
+            </li>
+          </ul>
+        </details>
+      </li>
+      <li><router-link to="/about">About</router-link></li>
+      <li><router-link to="/contact">Contact</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.menu {
+  justify-content: space-between;
+  align-content: center;
+  height: 9vh;
+}
+.header-logo {
+  display: inline-block;
+  width: 60%;
+  margin-top: 1rem;
+  place-self: center;
+}
+</style>
---
+++ src/components/navigation/NavMenu.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="navbar bg-base-100">
+    <div class="flex-1">
+      <img
+        class="header-logo"
+        src="../../assets/vanessasketch_logo.png"
+        alt="Vanessa Cohen - illustration and concept art"
+      />
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal px-1">
+        <li>
+          <details>
+            <summary>
+              <router-link to="/portfolio">Portfolio</router-link>
+            </summary>
+            <ul class="bg-base-100 rounded-t-none p-2 w-max">
+              <li>
+                <router-link to="/portfolio/personal">
+                  Personal Projects
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/portfolio/spongebob">
+                  SpongeBob's Idle Adventures
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/portfolio/environment"
+                  >Environmental Art
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/portfolio/hops?protected=true">
+                  Protected
+                </router-link>
+              </li>
+            </ul>
+          </details>
+        </li>
+        <li><router-link to="/about">About</router-link></li>
+        <li><router-link to="/contact">Contact</router-link></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.header-logo {
+  max-width: 50%;
+  max-height: 70px;
+  margin-left: 10px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+}
+</style>
