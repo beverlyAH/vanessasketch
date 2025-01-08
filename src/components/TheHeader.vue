be337b5f522639e3a970ba4e3b0253771050b449
Beverly Hernandez <beverlyandez@gmail.com>
2025-01-08 11:45:12 -0500

    add Header component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="bg-primary-color header">
+    <mobile-nav id="mobile-nav"></mobile-nav>
+    <nav-menu id="fullscreen-nav"></nav-menu>
+  </header>
+</template>
+
+<script setup>
+import MobileNav from "./navigation/MobileNav.vue";
+import NavMenu from "./navigation/NavMenu.vue";
+</script>
+
+<style scoped>
+header {
+  height: 12vh;
+  display: flex;
+  justify-content: center;
+}
+
+#mobile-nav {
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+}
+
+#fullscreen-nav {
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+}
+</style>
